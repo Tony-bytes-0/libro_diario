4071436a3c291c35dc96601f56e84726a187ced8
RylovNigami <mendezr1700@gmail.com>
2025-10-31 20:00:46 -0400

    rylov features: v-textfields

--- resources/js/pages/RegistroLibroDiario.vue
+++ resources/js/pages/RegistroLibroDiario.vue
@@ -1,20 +1,73 @@
 <template>
     <Head title="Registrar" />
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="d-flex py-4">
-            <v-row>
-                <v-col>
+        <div class="d-flex">
+            <v-row class="py-4 pr-6">
+                <v-col cols="4">
                     <v-date-input
                     v-model="registerDate"
-                    label="Indique la fecha"
-                    max-width="368"
+                    label="Fecha"
+
                     ></v-date-input>
                 </v-col>
-                <v-col>
+                <v-col cols="8">
                     <v-autocomplete
                     label="Nombre o Razon Social"
                     :items="clients"
+                    v-model="dataClient"
                     ></v-autocomplete>
+                </v-col>
+                <v-col cols="2">
+                    <v-text-field label="RIF"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-autocomplete
+                    label="Tipo de Documento"
+                    :items="docTypes"
+                    v-model="dataDoc"
+                    ></v-autocomplete>
+                </v-col>
+                <v-col>
+                    <v-text-field label="Registro Maquina Fiscal"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field label="Primera Factura"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field label="Ultima Factura"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field label="N° Factura (Compra, NC, ND)"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field label="Factura Afectada"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field label="Total Ventas (con I.V.A)"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field label="Ventas Internas no Agravadas"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field label="Base Imponible Alic (Gen - Contrib.)"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field label="% IVA"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field label="Impuesto I.V.A. (Contrib.)"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field label="Base Imponible Alic (Gen - No Contrib.)"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field label="% I.V.A."></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field label="Impuesto I.V.A. (No Contrib.)"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field label="Retencion I.V.A. Soportada"></v-text-field>
                 </v-col>
             </v-row>
         </div>
@@ -44,7 +97,6 @@ import { VDateInput } from 'vuetify/labs/VDateInput'
     'ADMINISTRADORA SOL Y MAR, SRL',
     'INSTALACIONES ELECTROHIDRAULICAS C.A.',
     'FUNDACION TEATRO LUIS MARIANO RIVERA',
-    'AKERMED C.A.',
     'R&R DIAGNOSTICO C.A.',
     'ALCALDIA BOLIVARIANA DEL MUNICIPIO MEJIA',
     'TRANSPORTE "EL FAMOSO"',
@@ -59,8 +111,23 @@ import { VDateInput } from 'vuetify/labs/VDateInput'
     'METALURGICA MANZANARES C.A.',
     'MASCOTIENDA CLINICA VETERINARIA C.A.',
     'SB, SA INGENIERA',
-    'U.E. INSTITUTO LIBERTADOR'
+    'U.E. INSTITUTO LIBERTADOR',
+    'U.E. INSTITUTO SAN LAZARO',
+    'ALUMINIOS PIPO C.A.',
+    'DISTRIBUIDORA LEMN C.A.',
+    'CONTRALORIA MUNICIPIO CRUZ SALMERON ACOSTA',
+    'MC VZLA',
+    'ECONOQUESOS PLAZA C.A.',
+    'DIORKA ORIENTE C.A.',
+    'LA PRIMOGENITA 6101, C.A.',
+    'HAKKO ENERGY C.A.',
+    'COSTO SUPERMARKET C.A.',
+    'CC CUMANA PLAZA C.A.',
+    'DEPILASER VE 2023 C.A.',
+    'RESUMEN DIARIO DE VENTAS'
 ];
+
+const values = ref('');
 
   const date = computed(() => {
     console.log(registerDate.value);
