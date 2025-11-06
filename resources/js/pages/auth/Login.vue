422d8ac90703a83169278776c8dccbdf03f18ea7
tony <tonygonzalezgarcia39@gmail.com>
2025-11-06 10:29:29 -0400

    texto login

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -20,87 +20,49 @@ defineProps<{
 </script>
 
 <template>
-    <AuthBase
-        title="Log in to your account"
-        description="Enter your email and password below to log in"
-    >
+    <AuthBase title="Inicia sesión!" description="Ingrese su correo y contraseña">
+
         <Head title="Log in" />
 
-        <div
-            v-if="status"
-            class="mb-4 text-center text-sm font-medium text-green-600"
-        >
+        <div v-if="status" class="mb-4 text-center text-sm font-medium text-green-600">
             {{ status }}
         </div>
 
-        <Form
-            v-bind="store.form()"
-            :reset-on-success="['password']"
-            v-slot="{ errors, processing }"
-            class="flex flex-col gap-6"
-        >
+        <Form v-bind="store.form()" :reset-on-success="['password']" v-slot="{ errors, processing }"
+            class="flex flex-col gap-6">
             <div class="grid gap-6">
                 <div class="grid gap-2">
-                    <Label for="email">Email address</Label>
-                    <Input
-                        id="email"
-                        type="email"
-                        name="email"
-                        required
-                        autofocus
-                        :tabindex="1"
-                        autocomplete="email"
-                        placeholder="email@example.com"
-                    />
+                    <Label for="email">Correo electronico</Label>
+                    <Input id="email" type="email" name="email" required autofocus :tabindex="1" autocomplete="email"
+                        placeholder="correo@ejemplo.com" />
                     <InputError :message="errors.email" />
                 </div>
 
                 <div class="grid gap-2">
                     <div class="flex items-center justify-between">
-                        <Label for="password">Password</Label>
-                        <TextLink
-                            v-if="canResetPassword"
-                            :href="request()"
-                            class="text-sm"
-                            :tabindex="5"
-                        >
+                        <Label for="password">Contraseña</Label>
+<!--                         <TextLink v-if="canResetPassword" :href="request()" class="text-sm" :tabindex="5">
                             Forgot password?
-                        </TextLink>
+                        </TextLink> -->
                     </div>
-                    <Input
-                        id="password"
-                        type="password"
-                        name="password"
-                        required
-                        :tabindex="2"
-                        autocomplete="current-password"
-                        placeholder="Password"
-                    />
+                    <Input id="password" type="password" name="password" required :tabindex="2"
+                        autocomplete="current-password" placeholder="Contraseña" />
                     <InputError :message="errors.password" />
                 </div>
 
                 <div class="flex items-center justify-between">
                     <Label for="remember" class="flex items-center space-x-3">
                         <Checkbox id="remember" name="remember" :tabindex="3" />
-                        <span>Remember me</span>
+                        <span>Recordar</span>
                     </Label>
                 </div>
 
-                <Button
-                    type="submit"
-                    class="mt-4 w-full"
-                    :tabindex="4"
-                    :disabled="processing"
-                    data-test="login-button"
-                >
-                    <LoaderCircle
-                        v-if="processing"
-                        class="h-4 w-4 animate-spin"
-                    />
-                    Log in
+                <Button type="submit" class="mt-4 w-full" :tabindex="4" :disabled="processing" data-test="login-button">
+                    <LoaderCircle v-if="processing" class="h-4 w-4 animate-spin" />
+                    Iniciar sesión
                 </Button>
             </div>
-
+            <!--
             <div
                 class="text-center text-sm text-muted-foreground"
                 v-if="canRegister"
@@ -108,6 +70,8 @@ defineProps<{
                 Don't have an account?
                 <TextLink :href="register()" :tabindex="5">Sign up</TextLink>
             </div>
+
+-->
         </Form>
     </AuthBase>
 </template>
