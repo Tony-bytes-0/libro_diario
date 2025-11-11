6b6007cee11ea0558b8b79948f5027e337b18542
tony <tonygonzalezgarcia39@gmail.com>
2025-11-10 21:20:21 -0400

    creacion de clientes

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cliente;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class ClientesController extends Controller
 {
-    public function view(){
+    public function view()
+    {
         return Inertia::render('administrar/clientes/Clientes');
     }
-    public function consultar(Request $request){
+    public function consultar(Request $request)
+    {
         $clients = DB::table('clientes')->select('*')->get();
 
         return response()->json([
@@ -20,20 +23,22 @@ class ClientesController extends Controller
         ]);
     }
 
-    public function crear(Request $request){
+    public function crear(Request $request)
+    {
         $validated = $request->validate([
             'rif' => 'required|string',
-             'descripcion' => 'required|string',
-             'tipo' => 'required|string',
-             'nombre' => 'nullable|string',
+            'descripcion' => 'required|string',
+            'tipo' => 'required|string',
+            'nombre' => 'nullable|string',
             'apellido' => 'nullable|string',
             'telefono' => 'nullable|string',
             'correo' => 'nullable|string',
         ]);
 
+        $result = Cliente::create($validated);
+        
         return response()->json(['msg' => 'Creado con éxito'], 201);
     }
-
 }
 
 /*
