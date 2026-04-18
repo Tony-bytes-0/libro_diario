78d26f532ebf90eb9d806b700017d7af38c9c497
Tony-bytes-0 <79365044+Tony-bytes-0@users.noreply.github.com>
2026-04-18 19:16:00 -0400

    Change primary key definition in clientes table

--- database/migrations/2025_10_31_224006_proveedores.php
+++ database/migrations/2025_10_31_224006_proveedores.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('clientes', function (Blueprint $table){
-            $table->increments('id')->primary();
+            $table->id();
             $table->string('rif')->nullable();
             $table->string('nombre')->nullable();
             $table->string('apellido')->nullable();
