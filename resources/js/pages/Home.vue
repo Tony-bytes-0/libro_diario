d1387765066d1ef40ae8baec7351eea104cdf7f3
Tony-bytes-0 <79365044+Tony-bytes-0@users.noreply.github.com>
2026-04-19 11:30:49 -0400

    Add Home.vue component with template and script

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Test Page</h1>
+        <p>If you see this, Inertia is working!</p>
+    </div>
+</template>
+
+<script setup>
+console.log('Home component loaded');
+</script>
